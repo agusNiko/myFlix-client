915d2ba252c39706330372e9c4a4365730539353
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-05-05 12:09:07 +0200

    cache

--- src/components/movie-view/MovieView.jsx
+++ src/components/movie-view/MovieView.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieView extends React.Component{
 
     render(){
-        const {movie, onBackClick} = this.props;
-        return (<div className="movie-view">
-            
-            <div className="movie-poster">
-                <img src={movie.ImagePath}/>
-            </div>
-          
-            
-            <div className = "movie-title">
-                <span className = "label">Title:</span>
-                <span className= "value">{movie.Title}</span>
-            </div>
 
-            <div className="movie-description">
-            <span className="value">{movie.Description}</span> 
-            </div>
-            <button onClick={() => { onBackClick(null); }}>Back</button>
-                
-            </div>
+        const {movie, onBackClick} = this.props;
+        return (
+            <Card style={{ width: '20rem' }} className="justify-content-md-center" className="movie-view">
+                <Card.Body>
+                    <Card.Img variant="top" src={movie.ImagePath} />
+                    <Card.Title>{movie.Title}</Card.Title>
+                    <Card.Text>{movie.Description}</Card.Text>
+                </Card.Body>
+                <Button variant="primary" onClick={()=> onBackClick(null)}>Back</Button>
+            </Card>
         );
     }
 }
