4a169bdf397a6f6dfcd00509a7763e4b54e0a889
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-08-04 13:44:55 +0200

    center the logIn form
    adding  empty <Col> around the form in every row

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,7 +8,7 @@ import InputGroup from 'react-bootstrap/InputGroup'
 
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col';
 
 
 import { Link } from "react-router-dom";
@@ -41,73 +41,67 @@ export function LoginView(props) {
 
   return (
 
-    <Container className="login-view">
-    <Row >
-        <Form className="login was-validated" noValidate >
+    <Container className="login-view mt-5">
+      <Row >
+        <Col></Col>
+        <Col>
+    
+          <Form className="login was-validated" noValidate >
             <Form.Group  controlId="formUsername">
               <Form.Label>
                 Username:
               </Form.Label>
               <Form.Control  
-              required
-              type="text" 
-              maxLength={20}
-              minLength={5}
-              placeholder="Enter username" 
-              value={username} 
-              onChange={e => setUsername(e.target.value)} />
-            <Form.Control.Feedback type="valid">
-              awesome you did it!
-            </Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              This field is mandatory!
-            </Form.Control.Feedback>
-          </Form.Group>
-    
-          <InputGroup hasValidation>
-      <Form.Group controlId="formPassword">
-        <Form.Label>
-        Password:
-        </Form.Label>
-        <Form.Control 
-          required 
-          type="password" 
-          placeholder="Enter your Password" 
-          value={password} 
-          onChange={e => setPassword(e.target.value)} />
-       <Form.Control.Feedback type="valid">
-          awesome you did it!
-        </Form.Control.Feedback>
-        <Form.Control.Feedback type="invalid">
-          This field is mandatory too!
-        </Form.Control.Feedback>
-      </Form.Group>
-      </InputGroup>
-          </Form>
-    </Row>
+                required
+                type="text" 
+                maxLength={20}
+                minLength={5}
+                placeholder="Enter username" 
+                value={username} 
+                onChange={e => setUsername(e.target.value)} />
+              <Form.Control.Feedback type="valid">
+                awesome you did it!
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                This field is mandatory!
+              </Form.Control.Feedback>
+            </Form.Group>
+            
+      
+              <Form.Group controlId="formPassword">
+                <Form.Label>
+                Password:
+                </Form.Label>
+                <Form.Control 
+                  required 
+                  type="password" 
+                  placeholder="Enter your Password" 
+                  value={password} 
+                  onChange={e => setPassword(e.target.value)} />
+                <Form.Control.Feedback type="valid">
+                  awesome you did it!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  This field is mandatory too!
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+        </Col>
+        <Col>
+        </Col>
+      </Row>
 
-    <Row>
-        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-      <Link to={`/register`}>
-        <Button  variant="secondary" type="submit">Register</Button>
-     </Link>
-    </Row>
-</Container>
-  //   <form>
-
- 
-
-
-  //     <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-
-
-
-  //     <Link to={`/register`}>
-  //       <Button  variant="secondary" type="submit">Register</Button>
-  //     </Link>
-
-
-  //   </form>
+      <Row>
+        <Col></Col>
+        <Col>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+          <Link to={`/register`}>
+            <Button  variant="secondary" type="submit">Register</Button>
+          </Link>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
 
   );
 }
